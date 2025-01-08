d979559ff868b8c6e1a3b0ecf8a8c02425e7d645
davidjmcclelland <davidjmcclelland@gmail.com>
2025-01-08 09:54:26 -0500

    initial commit

---
+++ lightStatus2.py
@@ -0,0 +1,51 @@
+# python 3.11
+
+import random
+import time
+
+from paho.mqtt import client as mqtt_client
+from paho import mqtt
+
+broker = 'e8cced050d434a399601fa55cd92b924.s2.eu.hivemq.cloud'
+port = 8883
+topic = "lightOn"
+
+# Generate a Client ID with the publish prefix.
+client_id = f'publish-{random.randint(0, 1000)}'
+username = 'raspberryPi_1'
+password = 'nXfU8rc*JAAr&f'
+
+def connect_mqtt():
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("return code "+str(rc)+"\n", rc)
+    print("lightStatus2.py")
+    client = mqtt_client.Client(client_id="", userdata="", protocol=mqtt_client.MQTTv5)
+    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def publish(client, payload):
+    time.sleep(1)
+    result = client.publish(topic, payload)
+    status = result[0]
+    if status == 0:
+        print(f"Send `{payload}` to topic `{topic}`")
+    else:
+        print(f"Failed to send message to topic {topic}")
+
+
+def run(payload):
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client, payload)
+    client.loop_stop()
+
+
+if __name__ == '__main__':
+    run(payload)
