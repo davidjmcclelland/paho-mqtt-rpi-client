0ac842ea0a8c366e0fb8d6f0f6fe40306d9e511b
David J McClelland <davidjmcclelland@gmail.com>
2025-02-06 16:55:08 -0500

    renamed script files

--- lightStatus.py
+++ lightStatus.py
@@ -6,9 +6,10 @@ import time
 from paho.mqtt import client as mqtt_client
 from paho import mqtt
 
-broker = 'e8cced050d434a399601fa55cd92b924.s2.eu.hivemq.cloud'
-port = 8883
-topic = "lightOn"
+# broker = 'e8cced050d434a399601fa55cd92b924.s2.eu.hivemq.cloud'
+broker = '192.168.1.50'
+port = 1883
+topic = "lightStatus"
 
 # Generate a Client ID with the publish prefix.
 client_id = f'publish-{random.randint(0, 1000)}'
@@ -21,11 +22,12 @@ def connect_mqtt():
             print("Connected to MQTT Broker!")
         else:
             print("return code "+str(rc)+"\n", rc)
-    print("lightStatus2.py")
-    client = mqtt_client.Client(client_id="", userdata="", protocol=mqtt_client.MQTTv5)
-    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    
+    client = mqtt_client.Client(client_id=client_id, userdata="true", protocol=mqtt_client.MQTTv5)
+    #client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
     client.username_pw_set(username, password)
     client.on_connect = on_connect
+    print("lightStatus2.py")
     client.connect(broker, port)
     return client
 
@@ -48,4 +50,5 @@ def run(payload):
 
 
 if __name__ == '__main__':
-    run(payload)
+    # run(payload)
+    run("true")
--- lightSwitch.py
+++ lightSwitch.py
@@ -1,15 +1,26 @@
-import RPi.GPIO as GPIO
 from time import sleep
+import RPi.GPIO as GPIO
+import lightStatus2 as lStatus
 
-GPIO.setmode(GPIO.BOARD)
-
+delay = 0.4
 inPin = 40
-
-GPIO.setup(inPin, GPIO.IN)
+outPin = 38
+cachedStatus = -1
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(outPin, GPIO.OUT)
+GPIO.setup(inPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 try:
     while True:
         readVal = GPIO.input(inPin)
-        print(readVal)
-        sleep(1)
+        print(f"readVal: `{readVal}`") #1
+        status = 1-readVal
+        print(f"cachedStatus: `{cachedStatus}` status: `{status}`")
+        GPIO.output(outPin,status)
+        if cachedStatus != status or cachedStatus == -1:
+            lStatus.run(status)
+            cachedStatus = status
+        # update MQTT lightStatus
+        sleep(delay)
 except KeyboardInterrupt:
     GPIO.cleanup()
+    print('GPIO clean')
